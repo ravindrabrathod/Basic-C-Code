05f2b3fba7828781322d2b964da9c892d6e13805
Ravindra Rathod <129941252+ravindrabrathod@users.noreply.github.com>
2025-06-16 21:26:57 +0530

    Add files via upload

---
+++ assignment operators.cpp
@@ -0,0 +1,73 @@
+// ASSIGNMENT OPERATORS :
+
+/*                                               // addition
+
+#include <stdio.h>
+main()
+{
+	int a=5;
+	a+=5;
+	printf("%d",a);		
+ } 
+ 
+                                                      //subtraction
+   
+   #include<stdio.h>
+   main()
+   {
+   	int a=5;
+   	a-=6;
+   	printf("%d",a);	
+   }
+   
+                                                  //Multiplication
+   
+   #include<stdio.h>
+   main()
+   {
+   	int a=5;
+   	a*=5;
+   	printf("%d",a);
+   }
+
+                                                 //Dividation
+   
+   #include<stdio.h>
+   main()
+   {
+   	int a=5;
+   	a/=5;
+   	printf("%d",a);
+   }
+      
+                                                  //Module %
+      
+      #include<stdio.h>
+      main()
+      {
+      	int a=5;
+      	a%=2;
+      	printf("%d",a);
+	  }
+  	
+	 */
+   
+    //WAP of +,-,*,/,% to understand the assignment operators
+	
+	#include<stdio.h>
+	main()
+	{
+		int a=100;
+		a+=50;
+		a-=50;
+		a*=10;
+		a/=5;
+		a%=10;
+		printf("%d",a);
+		
+	}
+	 
+	
+	 
+	 		      
+	                          
