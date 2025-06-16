8bdbaeadeb4b2dfe45428e73a5f1520d40be2f52
Ravindra Rathod <129941252+ravindrabrathod@users.noreply.github.com>
2025-06-16 23:37:54 +0530

    Add files via upload

---
+++ leap year.cpp
@@ -0,0 +1,14 @@
+
+// LEAP YEAR
+
+
+#include<stdio.h>
+main()
+{
+	int a;
+	printf("enter a year :");
+	scanf("%d",&a);
+	
+	printf("%s",a%4==0?"this is a leap year":"this is not a leap year");	
+
+}
