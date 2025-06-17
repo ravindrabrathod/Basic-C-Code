4db37c4015fb2e19aae29679d15924f4e64ed241
Ravindra Rathod <129941252+ravindrabrathod@users.noreply.github.com>
2025-06-17 14:59:12 +0530

    Add files via upload

---
+++ relational operator.cpp
@@ -0,0 +1,44 @@
+//RELATIONAL OPERATORS (<,>,<=,>=,==,!=)
+
+ 
+ #include<stdio.h>
+main()
+{
+	int a=16;
+	int b=8;
+	printf("%d",a>b);
+}
+
+
+#include<stdio.h>
+main()
+{
+	int a=5;
+	int b=8;
+	printf("%d",a<b);
+}
+
+#include<stdio.h>
+main()
+{
+	int a=5;
+	int b=8;
+	printf("%d",a!=b);
+}
+
+
+#include<stdio.h>
+main()
+{
+	int a=5;
+	int b=8;
+	printf("%d",a>=b);
+}
+
+#include<stdio.h>
+main()
+{
+	int a=5;
+	int b=8;
+	printf("%d",a<=b);
+}
