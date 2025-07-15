392fa86ff09772f397f8d390211f6db67727ea4d
Ravindra Rathod <129941252+ravindrabrathod@users.noreply.github.com>
2025-07-15 11:24:19 +0530

    Add files via upload

---
+++ ARRAY/1-D Array/Arrays 1D.cpp
@@ -0,0 +1,17 @@
+// MARKS PRINT ARRAYS
+
+#include<stdio.h>
+#define MARKS 5   // MACROS
+main()
+{
+	int a[MARKS],i;
+	for(i=0;i<MARKS;i++){
+	
+	printf("Enter the marks %d :",i+1);
+	scanf("%d",&a[i]);
+}
+for(i=0;i<MARKS;i++)
+
+	printf("%d\n",a[i]);
+}
+
